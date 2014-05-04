fe905818f3ef6c48f7b6f6fd389fbfcd22639680
Silvana Trindade <silvana@wheezy>
2014-05-04 20:59:23 -0300

    Plugin de docentes refeito

---
+++ plugins/docentes/docentes.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * Plugin Name: Docentes
+ */
+
+function mostra_html_docentes($post)
+{
+	$cargo = get_post_meta( $post->ID, "docentes_cargo", true );
+	$email = get_post_meta( $post->ID, "docentes_email", true );
+	$lattes = get_post_meta( $post->ID, "docentes_lattes", true );
+	$especialidade = get_post_meta( $post->ID, "docentes_especialidade", true );
+
+	echo <<<EOD
+
+<table>
+	<tbody>
+		<tr>
+			<td>Cargo:</td>
+			<td>
+				<input type="text" class="text" name="docentes_cargo" placeholder="Digite o cargo do docente" value="{$cargo}" />
+			</td>
+		</tr>
+		<tr>
+			<td>Email:</td>
+			<td>
+				<input type="email" class="text" name="docentes_email" placeholder="Digite o email do docente" value="{$email}" />
+			</td>
+		</tr>
+		<tr>
+			<td>Lattes:</td>
+			<td>
+				<input type="text" class="text" name="docentes_lattes" placeholder="Digite o lattes do docente" value="{$lattes}" />
+			</td>
+		</tr>
+		<tr>
+			<td>Especialidade:</td>
+			<td>
+				<input type="text" class="text" name="docentes_especialidade" placeholder="Digite a especialidade do docente" value="{$especialidade}" />
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+EOD;
+
+}
+
+function docentes_inicializa_metabox()
+{
+	add_meta_box( "metabox_info_docentes", 
+				  "Informações adicionais",
+				  "mostra_html_docentes",
+				  "docente",
+				  "advanced",
+				  "high");
+}
+
+function docentes_salva_dados($post_id)
+{
+	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+		return;
+	}
+
+	if (!current_user_can('edit_post' )) {
+		return;
+	}
+
+	$dadosPossiveis = array(
+		"docentes_cargo",
+		"docentes_email",
+		"docentes_lattes",
+		"docentes_especialidade"
+	);
+
+	foreach ($dadosPossiveis as $campo) {
+
+		if ( !isset($_POST[$campo]) ) {
+			continue;
+		}
+
+		$dado = esc_attr( $_POST[$campo] );
+
+		update_post_meta( $post_id, $campo, $dado );
+	}
+}
+
+function docentes_inicializa()
+{
+	register_post_type ( "docente", array(
+		"label" => "Docentes",
+		"public" => true,
+		"exclude_from_search" => false,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"menu_position" => 5,
+		"menu_icon" => "dashicons-admin-users",
+		"supports" => array( "title", "editor", "thumbnail" ),
+		"register_meta_box_cb" => "docentes_inicializa_metabox"
+	));
+
+	add_action( 'save_post', "docentes_salva_dados" );
+}
+
+add_action("init", "docentes_inicializa");
