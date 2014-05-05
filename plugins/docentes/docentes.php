4912cdf9eb7641e9f4020a56fcbf1f047339e48f
Silvana Trindade <silvana@wheezy>
2014-05-05 16:53:47 -0300

    Padronizando botões e renomeando o nome de alguns campos

--- plugins/docentes/docentes.php
+++ plugins/docentes/docentes.php
@@ -11,38 +11,38 @@ function mostra_html_docentes($post)
 	$lattes = get_post_meta( $post->ID, "docentes_lattes", true );
 	$especialidade = get_post_meta( $post->ID, "docentes_especialidade", true );
 
-	echo <<<EOD
+	echo <<<HTML
 
-<table>
+<table class="form-table">
 	<tbody>
 		<tr>
 			<td>Cargo:</td>
 			<td>
-				<input type="text" class="text" name="docentes_cargo" placeholder="Digite o cargo do docente" value="{$cargo}" />
+				<input type="text" class="large-text" name="docentes_cargo" placeholder="Digite o cargo do docente" value="{$cargo}" />
 			</td>
 		</tr>
 		<tr>
 			<td>Email:</td>
 			<td>
-				<input type="email" class="text" name="docentes_email" placeholder="Digite o email do docente" value="{$email}" />
+				<input type="email" class="large-text" name="docentes_email" placeholder="Digite o email do docente" value="{$email}" />
 			</td>
 		</tr>
 		<tr>
 			<td>Lattes:</td>
 			<td>
-				<input type="text" class="text" name="docentes_lattes" placeholder="Digite o lattes do docente" value="{$lattes}" />
+				<input type="text" class="large-text" name="docentes_lattes" placeholder="Digite o lattes do docente" value="{$lattes}" />
 			</td>
 		</tr>
 		<tr>
 			<td>Especialidade:</td>
 			<td>
-				<input type="text" class="text" name="docentes_especialidade" placeholder="Digite a especialidade do docente" value="{$especialidade}" />
+				<input type="text" class="large-text" name="docentes_especialidade" placeholder="Digite a especialidade do docente" value="{$especialidade}" />
 			</td>
 		</tr>
 	</tbody>
 </table>
 
-EOD;
+HTML;
 
 }
 
@@ -88,7 +88,24 @@ function docentes_salva_dados($post_id)
 function docentes_inicializa()
 {
 	register_post_type ( "docente", array(
-		"label" => "Docentes",
+		'labels' => array(
+
+      		'name'               =>  'Docentes',
+			'singular_name'      =>  'Docente',
+			'menu_name'          =>  'Docentes',
+			'name_admin_bar'     =>  'Docente' ,
+			'add_new'            =>  'Adicionar novo' ,
+			'add_new_item'       =>  'Adicionar novo Docente' ,
+			'new_item'           =>  'Novo Docente' ,
+			'edit_item'          =>  'Editar Docente',
+			'view_item'          =>  'Ver Docente',
+			'all_items'          =>  'Listagem de Docentes' ,
+			'search_items'       =>  'Buscar Docentes',
+			'parent_item_colon'  =>  'Docente pai:',
+			'not_found'          =>  'Nenhum docente encontrado.',
+			'not_found_in_trash' =>  'Nenhum docente encontrado na lixeira.',
+
+      	),
 		"public" => true,
 		"exclude_from_search" => false,
 		"publicly_queryable" => true,
