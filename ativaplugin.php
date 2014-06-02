25e83593e9a558772c689f3dcc7a88d6a63e6d93
Maurício André Cinelli <mauricio.andre.cinelli@gmail.com>
2014-06-02 19:54:21 -0300

    Corrigindo erro de ativação de plugin

--- ativaplugin.php
+++ ativaplugin.php
@@ -3,8 +3,20 @@
 require_once('wp-load.php' ); //not sure if this line is needed
 
 require_once(  'wp-admin/includes/plugin.php');
+require_once(  'wp-admin/includes/theme.php');
+
 $plugins = array("eventos","docentes","noticias","empregos","formandos","blog","cp");
+
 foreach ($plugins as $plugin){
-  $plugin_path = 'wp-content/plugins/{$plugin}.php';
+  $plugin_path = 'wp-content/plugins/{$plugin}/{$plugin}.php';
   activate_plugin($plugin_path);
 }
+
+/**
+ * Ativação de tema automático
+ *
+ * TODO: deve ser descomentado quando a estrutura de template for colocada
+ */
+
+// $tema = "sitecc";
+// switch_theme($tema, $tema);
