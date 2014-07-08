714a5579bbd7c9225815a32e5c6da731c3e776c5
Silvana Trindade <syletri@gmail.com>
2014-07-08 10:01:00 -0300

    Iniciando estilização da home

--- wp-content/themes/sitecc/templates/header-top-navbar.php
+++ wp-content/themes/sitecc/templates/header-top-navbar.php
@@ -25,17 +25,36 @@
     </nav>
 
     <?php if (is_front_page()): ?> 
-    <div class="slider-wrapper theme-default">
-      <div class="ribbon"></div>
-      <div id="slider" class="nivoSlider">
-        <img src="/assets/img/nemo.jpg" alt="" data-transition="slideInLeft" />
-        <img src="/assets/img/toystory.jpg" alt="" title="This is an example of a caption" data-transition="slideInLeft"/>
-        <img src="/assets/img/walle.jpg" alt="" data-transition="slideInLeft"/>
-      </div>
-      <div id="htmlcaption" class="nivo-html-caption">
-        <strong>This</strong> is an example of a <em>HTML</em> caption with <a href="#">a link</a>.
-      </div>
-    </div>
+      <?php 
+      $args = array(
+        'posts_per_page'   => 5,
+        'offset'           => 0,
+        'category'         => '',
+        'orderby'          => 'post_date',
+        'order'            => 'DESC',
+        'include'          => '',
+        'exclude'          => '',
+        'meta_key'         => '',
+        'meta_value'       => '',
+        'post_type'        => 'noticia',
+        'post_mime_type'   => '',
+        'post_parent'      => '',
+        'post_status'      => 'publish',
+        'suppress_filters' => true ); 
+      ?>
+     <?php $posts_array = get_posts( $args ); ?>
+      <?php if ($posts_array): ?>
+        <div class="slider-wrapper theme-default">
+          <div class="ribbon"></div>
+          <div id="slider" class="nivoSlider">
+            <?php foreach ($posts_array as $post): ?>
+              <?php if (!has_post_thumbnail($post->ID )) { continue; } ?>
+              <?php $src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) , 'full');?>
+              <img src="<?php echo $src[0]; ?>" alt="<?php echo $post->post_title; ?>" title="<?php echo $post->post_title; ?>" data-transition="slideInLeft">
+            <?php endforeach ?>
+          </div>
+        </div>
+      <?php endif ?>
     <?php endif ?>
   </div>
 </div>
--- wp-content/themes/sitecc/templates/pagina_inicial.php
+++ wp-content/themes/sitecc/templates/pagina_inicial.php
@@ -6,44 +6,188 @@ Template Name: Pรกgina Inicial
 ?>
 
 <div class="busca">
+	<div class="row">
+		<div class="col-xs-12 col-sm-6 col-sm-offset-6 col-md-4 col-md-offset-8 col-lg-3 col-lg-offset-9">
+			<input type="text" class="form-control">
+		</div>
+	</div>
 	
 </div>
 <div class="row">
 	
-	<div class="links-rapidos col-xs-12 col-sm-6 col-md-5 col-lg-4">
+	<div class="links-rapidos col-xs-12 col-sm-6 col-md-5  col-lg-4">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				
+				<a class="btn btn-lg btn-block btn-default link" href="http://www.uffs.edu.br/index.php">Site universidade</a>
 			</div>
 		</div>
 
 		<div class="row">
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-				
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<a class="btn btn-lg btn-block btn-default link" href="moodle.uffs.edu.br">Moodle</a>
 			</div>
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6"></div>
 		</div>
 		<div class="row">
 			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-				
+				<a class="btn btn-lg btn-block btn-default link" href="aluno.uffs.edu.br/login.xhtml">Portal do aluno</a>
 			</div>
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6"></div>
+			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+				<a class="btn btn-lg btn-block btn-default link" href="http://www.uffs.edu.br/index.php?option=com_content&view=category&layout=blog&id=274&Itemid=853&site=biblio">Biblioteca</a>
+			</div>
 		</div>
 	</div>
-	<div class="painel col-xs-12 col-sm-6 col-md-5 col-lg-4">
-		
+
+	<div class="painel col-xs-12 col-sm-6 col-md-7 col-lg-8 ">
+		<div class="clearfix painel-titulo">
+			<h3 class="pull-left">Notícias</h3>
+			<a href="" class="ver-mais pull-right">
+				ver mais <span class="glyphicon glyphicon-chevron-right"></span>
+			</a>
+		</div>
+		<?php 
+	      $args = array(
+	        'posts_per_page'   => 10,
+	        'offset'           => 0,
+	        'category'         => '',
+	        'orderby'          => 'post_date',
+	        'order'            => 'DESC',
+	        'include'          => '',
+	        'exclude'          => '',
+	        'meta_key'         => '',
+	        'meta_value'       => '',
+	        'post_type'        => 'noticia',
+	        'post_mime_type'   => '',
+	        'post_parent'      => '',
+	        'post_status'      => 'publish',
+	        'suppress_filters' => true ); 
+	      ?>
+	     <?php $posts_array = get_posts( $args ); ?>
+	      <?php if ($posts_array): $count = 0; ?>
+	          <table>
+	          	<tr>
+	            <?php foreach ($posts_array as $post): 
+	            	
+	            	if ($count === 2) {
+	            	echo "</tr><tr>"; $count=0;
+	            }?>
+	            <td>
+	              <a href="<?php echo post_permalink($post->ID);?>" title="<?php echo $post->post_title; ?>"><?php echo $post->post_title;?></a>
+	            </td>
+	            <?php $count = $count+1;endforeach ?>
+	            <?php 	            	
+	            	if ($count === 2) {
+	            	echo "</tr>"; $count=0;
+	            }?>
+	          </table>
+	      <?php endif ?>
 	</div>
 </div>
 <div class="row">
 	
-	<div class="painel  col-xs-12 col-sm-4 col-md-4 col-lg-4">
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, rerum, libero. Impedit, dolorem, modi saepe, corporis perspiciatis aperiam beatae iste rerum eligendi blanditiis veritatis. Ea blanditiis consequuntur dolore, repellat deleniti.</p>
+	<div class="painel col-xs-12 col-sm-4 col-md-4 col-lg-4 ">
+		<div class="clearfix painel-titulo">
+			<h3 class="pull-left">Blog</h3>
+			<a href="" class="ver-mais pull-right">
+				ver mais <span class="glyphicon glyphicon-chevron-right"></span>
+			</a>
+		</div>
+		<?php 
+	      $args = array(
+	        'posts_per_page'   => 5,
+	        'offset'           => 0,
+	        'category'         => '',
+	        'orderby'          => 'post_date',
+	        'order'            => 'DESC',
+	        'include'          => '',
+	        'exclude'          => '',
+	        'meta_key'         => '',
+	        'meta_value'       => '',
+	        'post_type'        => 'blog',
+	        'post_mime_type'   => '',
+	        'post_parent'      => '',
+	        'post_status'      => 'publish',
+	        'suppress_filters' => true ); 
+	      ?>
+	     <?php $posts_array = get_posts( $args ); ?>
+	      <?php if ($posts_array): ?>
+	          <ul >
+	            <?php foreach ($posts_array as $post): ?>
+	            <li>
+	              <a href="<?php echo post_permalink($post->ID);?>" title="<?php echo $post->post_title; ?>"><?php echo $post->post_title;?></a>
+	            </li>
+	            <?php endforeach ?>
+	          </ul>
+	      <?php endif ?>
 	</div>
-	<div class="painel  col-xs-12 col-sm-4 col-md-4 col-lg-4">
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, rerum, libero. Impedit, dolorem, modi saepe, corporis perspiciatis aperiam beatae iste rerum eligendi blanditiis veritatis. Ea blanditiis consequuntur dolore, repellat deleniti.</p>
+	<div class="painel col-xs-12 col-sm-4 col-md-4 col-lg-4 ">
+		<div class="clearfix painel-titulo">
+			<h3 class="pull-left">Eventos</h3>
+			<a href="" class="ver-mais pull-right">
+				ver mais <span class="glyphicon glyphicon-chevron-right"></span>
+			</a>
+		</div>
+		<?php 
+	      $args = array(
+	        'posts_per_page'   => 5,
+	        'offset'           => 0,
+	        'category'         => '',
+	        'orderby'          => 'post_date',
+	        'order'            => 'DESC',
+	        'include'          => '',
+	        'exclude'          => '',
+	        'meta_key'         => '',
+	        'meta_value'       => '',
+	        'post_type'        => 'evento',
+	        'post_mime_type'   => '',
+	        'post_parent'      => '',
+	        'post_status'      => 'publish',
+	        'suppress_filters' => true ); 
+	      ?>
+	     <?php $posts_array = get_posts( $args ); ?>
+	      <?php if ($posts_array): ?>
+	          <ul >
+	            <?php foreach ($posts_array as $post): ?>
+	            <li>
+	              <a href="<?php echo post_permalink($post->ID);?>" title="<?php echo $post->post_title; ?>"><?php echo $post->post_title;?></a>
+	            </li>
+	            <?php endforeach ?>
+	          </ul>
+	      <?php endif ?>
 	</div>
-	<div class="painel  col-xs-12 col-sm-4 col-md-4 col-lg-4">
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, rerum, libero. Impedit, dolorem, modi saepe, corporis perspiciatis aperiam beatae iste rerum eligendi blanditiis veritatis. Ea blanditiis consequuntur dolore, repellat deleniti.</p>
+	<div class="painel col-xs-12 col-sm-4 col-md-4 col-lg-4 ">
+		<div class="clearfix painel-titulo">
+			<h3 class="pull-left">Oportunidades</h3>
+			<a href="" class="ver-mais pull-right">
+				ver mais <span class="glyphicon glyphicon-chevron-right"></span>
+			</a>
+		</div>
+		<?php 
+	      $args = array(
+	        'posts_per_page'   => 5,
+	        'offset'           => 0,
+	        'category'         => '',
+	        'orderby'          => 'post_date',
+	        'order'            => 'DESC',
+	        'include'          => '',
+	        'exclude'          => '',
+	        'meta_key'         => '',
+	        'meta_value'       => '',
+	        'post_type'        => 'emprego',
+	        'post_mime_type'   => '',
+	        'post_parent'      => '',
+	        'post_status'      => 'publish',
+	        'suppress_filters' => true ); 
+	      ?>
+	     <?php $posts_array = get_posts( $args ); ?>
+	      <?php if ($posts_array): ?>
+	          <ul >
+	            <?php foreach ($posts_array as $post): ?>
+	            <li>
+	              <a href="<?php echo post_permalink($post->ID);?>" title="<?php echo $post->post_title; ?>"><?php echo $post->post_title;?></a>
+	            </li>
+	            <?php endforeach ?>
+	          </ul>
+	      <?php endif ?>
 	</div>
 
 </div>
