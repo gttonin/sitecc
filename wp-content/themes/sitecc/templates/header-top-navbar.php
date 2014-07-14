db2a76e48a4faebe0a240873bb958c947ff7b7e0
Maurício André Cinelli <mauricio.andre.cinelli@gmail.com>
2014-07-14 20:31:28 -0300

    Testando se existe pelo menos uma imagem para mostrar ao slider, pois se não tivesse nenhuma o menu quebrava

--- wp-content/themes/sitecc/templates/header-top-navbar.php
+++ wp-content/themes/sitecc/templates/header-top-navbar.php
@@ -1,4 +1,35 @@
-<div class="menu-padrao <?php echo is_front_page() ? "desloca" : ""; ?>" id='menu'>
+<?php 
+if (is_front_page()):
+  $args = array(
+    'posts_per_page'   => 5,
+    'offset'           => 0,
+    'category'         => '',
+    'orderby'          => 'post_date',
+    'order'            => 'DESC',
+    'include'          => '',
+    'exclude'          => '',
+    'meta_key'         => '',
+    'meta_value'       => '',
+    'post_type'        => 'noticia',
+    'post_mime_type'   => '',
+    'post_parent'      => '',
+    'post_status'      => 'publish',
+    'suppress_filters' => true ); 
+
+  $posts_array = get_posts( $args );
+  if ($posts_array): 
+    $temPeloMenosUmaImagem = false;
+    
+    foreach ($posts_array as $post):
+      if (has_post_thumbnail($post->ID )) { 
+        $temPeloMenosUmaImagem = true; 
+      }
+    endforeach;
+  endif;
+endif;
+?>
+
+<div class="menu-padrao <?php echo is_front_page() && $temPeloMenosUmaImagem ? "desloca" : ""; ?>" id='menu'>
   <div class="">
     <nav class="navbar navbar-default" role="navigation">
       <div class="container">
@@ -24,37 +55,17 @@
       </div><!-- /.container-fluid -->
     </nav>
 
-    <?php if (is_front_page()): ?> 
-      <?php 
-      $args = array(
-        'posts_per_page'   => 5,
-        'offset'           => 0,
-        'category'         => '',
-        'orderby'          => 'post_date',
-        'order'            => 'DESC',
-        'include'          => '',
-        'exclude'          => '',
-        'meta_key'         => '',
-        'meta_value'       => '',
-        'post_type'        => 'noticia',
-        'post_mime_type'   => '',
-        'post_parent'      => '',
-        'post_status'      => 'publish',
-        'suppress_filters' => true ); 
-      ?>
-     <?php $posts_array = get_posts( $args ); ?>
-      <?php if ($posts_array): ?>
-        <div class="slider-wrapper theme-default">
-          <div class="ribbon"></div>
-          <div id="slider" class="nivoSlider">
-            <?php foreach ($posts_array as $post): ?>
-              <?php if (!has_post_thumbnail($post->ID )) { continue; } ?>
-              <?php $src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) , 'full');?>
-              <img src="<?php echo $src[0]; ?>" alt="<?php echo $post->post_title; ?>" title="<?php echo $post->post_title; ?>" data-transition="slideInLeft">
-            <?php endforeach ?>
-          </div>
-        </div>
-      <?php endif ?>
+    <?php if (is_front_page() && $posts_array && $temPeloMenosUmaImagem): ?> 
+    <div class="slider-wrapper theme-default">
+      <div class="ribbon"></div>
+      <div id="slider" class="nivoSlider">
+        <?php foreach ($posts_array as $post): ?>
+          <?php if (!has_post_thumbnail($post->ID )) { continue; } ?>
+          <?php $src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) , 'full');?>
+          <img src="<?php echo $src[0]; ?>" alt="<?php echo $post->post_title; ?>" title="<?php echo $post->post_title; ?>" data-transition="slideInLeft">
+        <?php endforeach ?>
+      </div>
+    </div>
     <?php endif ?>
   </div>
 </div>
