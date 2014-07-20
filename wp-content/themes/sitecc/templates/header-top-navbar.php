78149b00187118e1c68436013e75e7c42dc8a55b
Silvana Trindade <syletri@gmail.com>
2014-07-20 20:50:01 -0300

    Adicionando correções: link para logo,layout das páginas estáticas

--- wp-content/themes/sitecc/templates/header-top-navbar.php
+++ wp-content/themes/sitecc/templates/header-top-navbar.php
@@ -41,7 +41,7 @@ endif;
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#"><img src="/assets/img/logo_cc.png"></a>
+          <a class="navbar-brand" href="<?php echo get_site_url(); ?>"><img src="/assets/img/logo_cc.png"></a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
