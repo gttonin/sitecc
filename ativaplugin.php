1bd85755a8cdeef4b956234dadd53a9479253862
Maurício André Cinelli <mauricio.andre.cinelli@gmail.com>
2014-07-25 20:30:50 -0300

    Testando deploy

--- ativaplugin.php
+++ ativaplugin.php
@@ -7,7 +7,7 @@ require_once( $baseDir . 'wp-load.php' ); //not sure if this line is needed
 require_once( $baseDir . 'wp-admin/includes/plugin.php');
 require_once( $baseDir . 'wp-admin/includes/theme.php');
 
-$plugins = array("eventos","docentes","noticias","empregos","formandos","blog","clube-de-programacao");
+$plugins = array("eventos","docentes","noticias","oportunidades","formandos","blog","clube-de-programacao");
 
 foreach ($plugins as $plugin){
   $plugin_path = $baseDir . "wp-content/plugins/{$plugin}/{$plugin}.php";
