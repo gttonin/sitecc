b3c339d4f9b6a3a90aef04953d2770526740a6f2
Maurício André Cinelli <mauricio.andre.cinelli@gmail.com>
2014-07-25 21:07:02 -0300

    iniciando busca

---
+++ wp-content/themes/sitecc/templates/resultado_busca.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Template Name: Resultado de Busca
+*/
+
+$term = esc_attr($_GET['s']);
+
+$query = new WP_Query(array(
+  "post_type" => array("noticia", "blog"),
+  "post_status" => "publish",
+  "s" => $term
+));
+
+?>
+
+<h2>Resultados da busca por "<?php echo $term ?>"</h2>
+
+<?php if ($query->posts): ?>
+  <?php foreach ($query->posts as $resultado): ?>
+    
+  <?php endforeach ?>
+<?php else: ?>
+  <h5 class="text-center">Nenhum resultado encontrado...</h5>
+<?php endif ?>
