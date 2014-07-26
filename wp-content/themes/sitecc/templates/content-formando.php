4bb8f978c0a2f27b6b09604e1cf9eaf1afca77b3
Silvana Trindade <syletri@gmail.com>
2014-07-25 22:30:27 -0300

    Finalizando formandos

--- wp-content/themes/sitecc/templates/content-formando.php
+++ wp-content/themes/sitecc/templates/content-formando.php
@@ -22,7 +22,7 @@ foreach ($anos as $ano) {
   echo "<div class='slider-formandos'>";
   foreach ($formandos->posts as $formando) {
     echo "<div>";
-    echo get_the_post_thumbnail($formando->ID, "medium",array('class'=>'imagem-capa'));
+    echo get_the_post_thumbnail($formando->ID, "medium",array('class'=>'imagem-capa','data-toggle'=>'tooltip','data-placement'=>'bottom','title'=>$formando->post_title));
     echo "</div>";
   }
   echo "</div>";
