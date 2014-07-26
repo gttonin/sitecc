3d63d1cf1b8879dafcdb2307599cc03afd14ff1e
Maurício André Cinelli <mauricio.andre.cinelli@gmail.com>
2014-07-26 15:55:44 -0300

    Corrigindo anexos

--- wp-content/themes/sitecc/templates/content-page.php
+++ wp-content/themes/sitecc/templates/content-page.php
@@ -55,24 +55,7 @@
       <?php endif; ?>
     </div>
 
-    <div class="anexos">
-    <?php $attachments = new Attachments( 'anexos' ); /* pass the instance name */ ?>
-    <?php if( $attachments->exist() ) : ?>
-      <h3>Anexos</h3>
-      <ul class="clearfix">
-        <?php while( $attachments->get() ) : ?>
-          <li class="anexo clearfix">
-            <img src="<?php echo get_site_url() ?>/assets/img/file.png" alt="Anexo" />
-            
-            <div class="anexo-descricao">
-              <p><?php echo $attachments->field( 'titulo' ); ?></p>
-              <p><a href="<?php echo $attachments->url(); ?>" class="download" target="_blank">Baixar anexo</a></p>
-            </div>
-          </li>
-        <?php endwhile; ?>
-      </ul>
-    <?php endif; ?>
-    </div>
+    <?php include "anexos.php"; ?>
   </div>
 <?php
 if ($args) {
--- wp-content/themes/sitecc/templates/content-single-noticia.php
+++ wp-content/themes/sitecc/templates/content-single-noticia.php
@@ -15,24 +15,7 @@
       <?php the_content(); ?>
     </div>
 
-    <div class="anexos">
-    <?php $attachments = new Attachments( 'anexos' ); /* pass the instance name */ ?>
-    <?php if( $attachments->exist() ) : ?>
-      <h3>Anexos</h3>
-      <ul class="clearfix">
-        <?php while( $attachments->get() ) : ?>
-          <li class="anexo clearfix">
-            <img src="<?php echo get_site_url() ?>/assets/img/file.png" alt="Anexo" />
-            
-            <div class="anexo-descricao">
-              <p><?php echo $attachments->field( 'titulo' ); ?></p>
-              <p><a href="<?php echo $attachments->url(); ?>" class="download" target="_blank">Baixar anexo</a></p>
-            </div>
-          </li>
-        <?php endwhile; ?>
-      </ul>
-    <?php endif; ?>
-    </div>
+    <?php include "anexos.php"; ?>
 
     <div class="comentarios">
       <?php echo disqus_embed("sitedecinciadacomputaouffs"); ?>
